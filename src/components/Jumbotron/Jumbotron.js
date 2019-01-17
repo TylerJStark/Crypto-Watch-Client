6ea9ff3c2581c027a3b4b751a66655b487fc9d61
Dova <Tyler4276@gmail.com>
2019-01-16 19:57:19 -0500

    Joe: Posting unknown changes here so we can post to heroku on master

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,11 +3,11 @@ import "./Jumbotron.css";
 
 const Jumbotron = props => (
   
-  <div class="jumbotron jumbotron-fluid background">
-    <div class="container">
+  <div className="jumbotron jumbotron-fluid background">
+    <div className="container">
     <img src="/assets/media/Crypto-Watch-logo.png"/>
-      {/* <h1 class="display-4 text-center">Crypto-Watch</h1> */}
-      <p class="lead text-center"></p>
+      {/* <h1 className="display-4 text-center">Crypto-Watch</h1> */}
+      <p className="lead text-center"></p>
     </div>
   </div>
 )
